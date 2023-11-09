831891dc3c11ca24ffe6044bf5876199a9157066
antukbalaTL <amit.kumar@trucklagbe.com>
2023-11-09 19:17:31 +0600

    calculateDistanceByHaversineFormula function updated, variable names updated

---
+++ services/distanceCalculation.js
@@ -0,0 +1,24 @@
+function calculateDistanceByHaversineFormula(targetLatitude, targetLongitude, userLatitude, userLongitude, unit='km') {
+    try {
+	    const RADIUS_OF_EARTH = 6371;
+        const DEGREE_TO_RADIAN = Math.PI / 180;
+	    const latitudeDifference = (targetLatitude - userLatitude) * DEGREE_TO_RADIAN;
+	    const longitudeDifference = (targetLongitude - userLongitude) * DEGREE_TO_RADIAN;
+        const haversonLatitudeAngle = Math.sin(latitudeDifference / 2) * Math.sin(latitudeDifference / 2);
+        const haversonLongitudeAngle = Math.sin(longitudeDifference / 2) * Math.sin(longitudeDifference / 2);
+        const targetLatitudeInRadian = targetLatitude * (DEGREE_TO_RADIAN);
+        const userLatitudeInRadian = userLatitude * (DEGREE_TO_RADIAN);
+	
+	    const a = haversonLatitudeAngle + Math.cos(targetLatitudeInRadian) * Math.cos(userLatitudeInRadian) * haversonLongitudeAngle;
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+	    const distanceInMeter = (unit === 'm') ? RADIUS_OF_EARTH * c * 1000 : RADIUS_OF_EARTH * c;
+	
+	    return distanceInMeter;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+console.log(calculateDistanceByHaversineFormula(23.7792704, 90.3801065, 23.7792745, 90.3808767, 'm'));
+  
